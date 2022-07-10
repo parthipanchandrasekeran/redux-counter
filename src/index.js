0421c05b3c718884c3bef2c8367779695bd1a42c
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-07-10 14:55:58 -0400

    counter added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+/*//STORE -- GLOBALISED STATE
+
+//ACTION -- INCREMENT - FUNC WHICH RETURNS AN OBJECT
+
+const increment = () => {
+  return { type: "INCREMENT" };
+};
+
+const decrement = () => {
+  return { type: "DECREMENT" };
+};
+
+//REDUCER -- ACTIONS TRANSFORM THE STATE INTO NEW STATE, STATE CAN BE STORED AS AN OBJECT AND SENT
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+//display in console
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+//DISPATCH -- ACTION TO THE REDUCER
+
+store.dispatch(increment());
+store.dispatch(decrement());*/
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
